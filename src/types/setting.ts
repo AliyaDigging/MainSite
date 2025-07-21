85e7f60c0c8a9df07d7d930bbdb712297ea9781f
sctop <26916108+sctop@users.noreply.github.com>
2025-07-21 14:16:23 +0800

    fix: 修改 L10nLang 的定义

--- src/types/setting.ts
+++ src/types/setting.ts
@@ -1,5 +1,5 @@
 export type SiteLanguage = 'zh_CN' | 'en_US'
 export const SiteLanguage: SiteLanguage[] = ['zh_CN', 'en_US']
 
-export type L10nLanguage = 'zh-cn' | 'en' | 'zh-cn-c'
-export const L10nLanguage: L10nLanguage[] = ['zh-cn', 'zh-cn-c', 'en']
+export type L10nLanguage = 'zh-cn' | 'en-us' | 'zh-cn-c'
+export const L10nLanguage: L10nLanguage[] = ['zh-cn', 'zh-cn-c', 'en-us']
