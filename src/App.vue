0f6ccc630be76a857b4185e6708b74cca8a6a114
sctop <26916108+sctop@users.noreply.github.com>
2026-06-27 13:12:55 +0800

    feat: 新增全局顶层免责声明

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,33 @@ import NavMenu from '@/components/NavMenu.vue'
 import FooterComp from './components/FooterComp.vue'
 
 import DynamicDialog from 'primevue/dynamicdialog'
+import Message from 'primevue/message'
 import { useSiteSettingStore } from './stores/setting'
-import { watch, inject, computed } from 'vue'
+import { watch, inject, computed, ref } from 'vue'
 
 import { symbolUseDark } from '@/constants/injection'
+import { Button } from 'primevue'
 
 const setting = useSiteSettingStore()
 
 const isDark = inject(symbolUseDark)!
 const cssCodeBgColor = computed(() => (isDark.value ? '#4a1c1f' : '#fff5f5'))
 const cssCodeTextColor = computed(() => (isDark.value ? '#ff6b6b' : '#dc3545'))
+
+const shouldDisplayWarning = ref(
+  (() => {
+    const temp = localStorage.getItem('top-view-warning-1')
+    if (!temp && temp !== 'closed') return true
+    return false
+  })(),
+)
+function setWarningCloseState() {
+  localStorage.setItem('top-view-warning-1', 'closed')
+  shouldDisplayWarning.value = false
+}
+function doAlert(content: string) {
+  alert(content)
+}
 
 watch(
   () => setting.sitelang,
@@ -24,11 +41,34 @@ watch(
 <template>
   <div>
     <DynamicDialog />
+    <Message
+      severity="warn"
+      icon="pi pi-exclamation-triangle"
+      style="text-align: left"
+      v-if="shouldDisplayWarning"
+    >
+      <div style="margin-left: 4px">
+        <p style="font-size: 1.5rem; font-weight: bold">{{ $t('view.global.warning.1.title') }}</p>
+        <p v-html="$t('view.global.warning.1.p.1')"></p>
+        <p v-html="$t('view.global.warning.1.p.2')"></p>
+        <p v-html="$t('view.global.warning.1.p.3')"></p>
+        <div class="mt-2">
+          <Button @click="setWarningCloseState" severity="success">{{
+            $t('view.global.warning.1.btn.1')
+          }}</Button
+          >&emsp;<Button
+            @click="doAlert($t(`view.global.warning.1.btn.2.alert`))"
+            severity="danger"
+            >{{ $t('view.global.warning.1.btn.2') }}</Button
+          >
+        </div>
+      </div>
+    </Message>
 
-    <div style="position: fixed; top: 0; width: 100%; z-index: 10">
+    <div style="position: sticky; top: 0; width: 100%; z-index: 10">
       <NavMenu />
     </div>
-    <div style="margin-top: 58px">
+    <div style="">
       <RouterView />
     </div>
     <FooterComp />
@@ -44,10 +84,10 @@ code {
   color: v-bind(cssCodeTextColor);
   white-space: pre-wrap;
   word-wrap: break-word;
-
-  margin-top: 2px;
-  display: inline-block;
+  line-height: 1.8em;
 }
+
+/** 这下面的代码好像不起效果，但为了防止bug还是保留了 */
 :deep(code) {
   font-family: 'Noto Sans Mono', monospace !important;
   background-color: v-bind(cssCodeBgColor);
@@ -56,5 +96,9 @@ code {
   color: v-bind(cssCodeTextColor);
   white-space: pre-wrap;
   word-wrap: break-word;
+
+  margin-top: 2px;
+  display: inline;
+  line-height: 1.8em;
 }
 </style>
